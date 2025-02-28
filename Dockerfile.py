f24f9fd20db3fa0647b1464bed9438ae83faaa75
js4000all <js4000all@gmail.com>
2025-03-01 08:57:12 +0900

    add additional Python packages for MySQL support, type checking, and testing in Dockerfile

--- Dockerfile.py
+++ Dockerfile.py
@@ -2,8 +2,9 @@ FROM python:alpine
 RUN set -x \
     && apk add curl git gcc musl-dev linux-headers \
     && python3 -m pip install influxdb-client \
-    && python3 -m pip install mysql-connector-python pymysql \
+    && python3 -m pip install mysql-connector-python pymysql types-PyMySQL \
     && python3 -m pip install jupyterlab \
+    && python3 -m pip install mypy pytest \
     && addgroup -S appgroup \
     && adduser -S appuser -G appgroup
 
